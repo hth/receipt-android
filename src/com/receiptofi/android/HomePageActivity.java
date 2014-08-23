e4c350d994b3ed5189221fa600c2a14008e314e4
Hitender Tholia <hitender@tholix.com>
2014-08-23 11:57:19 -0700

    rename and code formatting

--- src/com/receiptofi/android/HomePageActivity.java
+++ src/com/receiptofi/android/HomePageActivity.java
@@ -8,12 +8,12 @@ import android.provider.MediaStore;
 import android.view.View;
 
 import com.receiptofi.android.http.API;
-import com.receiptofi.android.http.HTTPutils;
+import com.receiptofi.android.http.HTTPUtils;
 
 public class HomePageActivity extends ParentActivity {
 
-	private static final int RESULT_IMAGE_GALLARY=0x4c5;
-	private static final int RESULT_IMAGE_CAPTURE=0x4c6;
+	private static final int RESULT_IMAGE_GALLARY = 0x4c5;
+	private static final int RESULT_IMAGE_CAPTURE = 0x4c6;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,7 @@ public class HomePageActivity extends ParentActivity {
 		startActivity(new Intent(this, SettingsPage.class));
 		this.overridePendingTransition(R.anim.left_slide_in, R.anim.right_side_out);
 	}
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
@@ -36,46 +37,43 @@ public class HomePageActivity extends ParentActivity {
 	public void takePhoto(View view){
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		startActivity(intent);
-	
 	}
 	
 	public void chooseImage(View view){
 		Intent g = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);	
 		startActivityForResult(g, RESULT_IMAGE_GALLARY);
 	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if(requestCode==RESULT_IMAGE_GALLARY && resultCode==RESULT_OK && data!=null){
-			
-			Uri imageGallary = data.getData();
-			String[] filePathColoumn = { MediaStore.Images.Media.DATA };
-			Cursor c = getContentResolver().query(imageGallary,
-					filePathColoumn, null, null, null);
-			c.moveToFirst();
-			final String imageAbsolutePath = c.getString(c
-					.getColumnIndex(filePathColoumn[0]));
-			try {
-				new Thread(){
-					public void run() {
-						try {
-						String str=	HTTPutils.uploadImage(API.UPLOAD_IMAGE_API,imageAbsolutePath);
-						showErrorMsg(str);
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					};
-				}.start();
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_IMAGE_GALLARY && resultCode == RESULT_OK && data != null) {
+
+            Uri imageGallery = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(imageGallery, filePathColumn, null, null, null);
+            c.moveToFirst();
+            final String imageAbsolutePath = c.getString(c.getColumnIndex(filePathColumn[0]));
+            try {
+                new Thread() {
+                    public void run() {
+                        try {
+                            String str = HTTPUtils.uploadImage(API.UPLOAD_IMAGE_API, imageAbsolutePath);
+                            showErrorMsg(str);
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
