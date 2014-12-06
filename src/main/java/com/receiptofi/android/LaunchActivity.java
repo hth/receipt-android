886b059e5d099ec537feb1a9dd7e0c633e063029
Suman <sersoun@gmail.com>
2014-12-06 15:28:10 -0800

    Change to Launch Activity

--- src/main/java/com/receiptofi/android/LaunchActivity.java
+++ src/main/java/com/receiptofi/android/LaunchActivity.java
@@ -4,7 +4,6 @@ import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -27,15 +26,10 @@ import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnFocusChangeListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -61,14 +55,13 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
     private static final int GOOGLE_PLUS_SIGN_IN = 0x2565;
     private static final int FACEBOOK_SIGN_IN = 0x2566;
 
-    private TextView signupText;
     private GoogleApiClient mGoogleApiClient;
     private ConnectionResult mConnectionResult;
-    private Button facebookLogin;
-    private boolean isFbLoginClick = false;
-    private boolean mSignInClicked;
+    private LinearLayout mGooglePlusLogin;
+    private LinearLayout mFacebookLogin;
+    private boolean isFbLoginClicked = false;
+    private boolean isGPlusLoginClicked;
     private boolean mIntentInProgress;
-    private SignInButton googlePlusLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,22 +72,18 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
             finish();
         }
         setContentView(R.layout.launch_page);
-        signupText = (TextView) findViewById(R.id.signupText);
 
-        googlePlusLogin = (SignInButton) findViewById(R.id.loginGooglePlus);
 
-        googlePlusLogin.setOnClickListener(this);
-        facebookLogin = (Button) findViewById(R.id.loginFacebook);
-        facebookLogin.setOnClickListener(this);
-
-        signupText
-                .setText(Html
-                        .fromHtml("Don't have an account? <u><font color=\"blue\">Sign up</font></u>, it's free!"));
+        mGooglePlusLogin = (LinearLayout) findViewById(R.id.google_login);
+        mGooglePlusLogin.setOnClickListener(this);
+        mFacebookLogin = (LinearLayout) findViewById(R.id.facebook_login);
+        mFacebookLogin.setOnClickListener(this);
         // Initializing google plus api client
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+       /* mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this).addApi(Plus.API)
                 .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+                */
     }
 
 
@@ -114,104 +103,27 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
         }
     }
 
-    private static OnFocusChangeListener editTextListener = new OnFocusChangeListener() {
-
-        @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-            // TODO Auto-generated method stub
-
-            if (hasFocus) {
-                ((EditText) v).setHint("");
-            } else {
-                String input = ((EditText) v).getText().toString();
-                if (input != null && input.length() == 0) {
-                    int id = v.getId();
-                    if (id == R.id.userName) {
-                        ((EditText) v).setHint("Email");
-                    } else if (id == R.id.password) {
-                        ((EditText) v).setHint("Password");
-                    }
-                }
-            }
-
-        }
-
-    };
-
-    public void login(View view) {
-        // getting username and password
-        userNameStr = userName.getText().toString();
-        passwordStr = password.getText().toString();
-
-        if (userNameStr == null || (userNameStr != null && userNameStr.length() == 0)) {
-            errors.append(this.getResources().getString(R.string.enter_email));
-        } else {
-            if (!UserUtils.isValidEmail(userNameStr)) {
-                addErrorMsg(this.getResources().getString(R.string.enter_valid_email));
-            }
-        }
-        if (passwordStr == null || (passwordStr != null && passwordStr.length() == 0)) {
-            addErrorMsg(this.getResources().getString(R.string.enter_password));
-        }
-        // error string is for keeping the error that needs to be shown to the
-        // user.
-        if (errors.length() > 0) {
-            Toast toast = Toast.makeText(this, errors, Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.TOP, 0, 20);
-            toast.show();
-            errors.delete(0, errors.length());
-        } else {
-            authenticateUser(false, null);
-        }
-    }
-
-    private void launchHomeScreen() {
-
-        uiThread.post(new Runnable() {
-
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                finish();
-                startActivity(new Intent(LaunchActivity.this, HomePageActivity.class));
-                LaunchActivity.this.overridePendingTransition(R.anim.right_slide_in, R.anim.left_slide_out);
-            }
-        });
-
-    }
-
-    private void addErrorMsg(String msg) {
-        if (errors.length() == 0) {
-            errors.append(msg);
-        } else {
-            errors.append("\n").append("\n").append(msg);
-        }
-    }
-
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
         addToBackStack(this);
 
-        if (isFbLoginClick) {
-            isFbLoginClick = false;
+        if (isFbLoginClicked) {
+            isFbLoginClicked = false;
             Session s = Session.getActiveSession();
             if (s != null && s.isOpened()) {
                 Bundle data = new Bundle();
                 data.putString(key.ACCESS_TOKEN, s.getAccessToken());
                 data.putString(key.PID, key.PID_FACEBOOK);
-                authenticateUser(true, data);
-
+                authenticateUser(data);
             }
         }
 
     }
 
-    private void authenticateUser(boolean isSocialLogin, Bundle data) {
-        if (isSocialLogin) {
-
-            showLoader(this.getResources().getString(R.string.login_auth_msg));
+    private void authenticateUser(Bundle data) {
+        showLoader(this.getResources().getString(R.string.login_auth_msg));
 
             JSONObject postData = new JSONObject();
 
@@ -219,7 +131,6 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
                 postData.put(API.key.PID, data.getString(key.PID));
                 postData.put(API.key.ACCESS_TOKEN, data.getString(key.ACCESS_TOKEN));
             } catch (JSONException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
 
@@ -245,58 +156,38 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
                 }
             });
 
-
-        } else {
-
-            showLoader(this.getResources().getString(R.string.login_auth_msg));
-
-            final ArrayList<NameValuePair> pairs = new ArrayList<NameValuePair>();
-            pairs.add(new BasicNameValuePair("mail", userNameStr));
-            pairs.add(new BasicNameValuePair("password", passwordStr));
-
-            new Thread() {
-                @Override
-                public void run() {
-                    // TODO Auto-generated method stub
-                    super.run();
-                    Header[] headers;
-                    try {
-                        headers = HTTPUtils.getHTTPHeaders(pairs, API.LOGIN_API);
-                    } catch (Exception e) {
-                        // TODO Auto-generated catch block
-                        hideLoader();
-                        if (e instanceof IOException)
-                            showErrorMsg("Please check your network connection");
-                        return;
-                    }
-
-                    hideLoader();
-                    if (headers != null) {
-                        for (Header header : headers) {
-                            String key = header.getName();
-                            if (key != null && (key.trim().equals(API.key.XR_MAIL) || key.trim().equals(API.key.XR_AUTH))) {
-                                String value = header.getValue();
-                                KeyValue.insertKeyValue(LaunchActivity.this, key, value);
-                            }
-                        }
-                    }
-                    afterSuccessfullLogin();
-                }
-            }.start();
-
-        }
     }
 
     @Override
     public void onClick(View v) {
         // TODO Auto-generated method stub
         switch (v.getId()) {
-            case R.id.loginGooglePlus:
-                // Signin button clicked
+            case R.id.google_login:
+                isGPlusLoginClicked = true;
                 signInWithGplus();
                 break;
-            case R.id.loginFacebook:
-                isFbLoginClick = true;
+            case R.id.google_logo:
+                isGPlusLoginClicked = true;
+                signInWithGplus();
+                break;
+            case R.id.facebook_login:
+
+                isFbLoginClicked = true;
+                Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+                    // callback when session changes state
+                    @Override
+                    public void call(Session session, SessionState state,
+                                     Exception exception) {
+                        if (session.isOpened()) {
+
+                        }
+                    }
+                });
+
+                break;
+            case R.id.facebook_logo:
+                isFbLoginClicked = true;
                 Session.openActiveSession(this, true, new Session.StatusCallback() {
 
                     // callback when session changes state
@@ -315,8 +206,13 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
     }
 
     private void signInWithGplus() {
+        // Initializing google plus api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
         if (!mGoogleApiClient.isConnecting()) {
-            mSignInClicked = true;
+            isGPlusLoginClicked = true;
             resolveSignInError();
         }
     }
@@ -328,6 +224,20 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
         } else {
             showErrorMsg("Login Failed !!!");
         }
+    }
+
+    private void launchHomeScreen() {
+
+        uiThread.post(new Runnable() {
+
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(LaunchActivity.this, HomePageActivity.class));
+                LaunchActivity.this.overridePendingTransition(R.anim.right_slide_in, R.anim.left_slide_out);
+            }
+        });
+
     }
 
     private void resolveSignInError() {
@@ -348,7 +258,7 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GOOGLE_PLUS_SIGN_IN) {
             if (resultCode != RESULT_OK) {
-                mSignInClicked = false;
+                isGPlusLoginClicked = false;
             }
 
             mIntentInProgress = false;
@@ -374,7 +284,7 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
             // Store the ConnectionResult for later usage
             mConnectionResult = result;
 
-            if (mSignInClicked) {
+            if (isGPlusLoginClicked) {
                 // The user has already clicked 'sign-in' so we attempt to
                 // resolve all
                 // errors until the user is signed in, or they cancel.
@@ -385,8 +295,7 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        // TODO Auto-generated method stub
-        mSignInClicked = false;
+        isGPlusLoginClicked = false;
         Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
 
         // Get user's information
@@ -402,7 +311,6 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
     }
 
     private void getUserInformation() {
-        // TODO Auto-generated method stub
         AccessTokenGooglePlus gToken = new AccessTokenGooglePlus();
         gToken.execute((Void) null);
     }
@@ -446,7 +354,7 @@ public class LaunchActivity extends ParentActivity implements OnClickListener, C
                 Bundle data = new Bundle();
                 data.putString(key.PID, key.PID_GOOGLE);
                 data.putString(key.ACCESS_TOKEN, token);
-                authenticateUser(true, data);
+                authenticateUser(data);
             } else {
                 Log.i("TOKEN IS  NULL MAKING QUERY", "TOKEN IS  NULL MAKING QUERY");
             }
