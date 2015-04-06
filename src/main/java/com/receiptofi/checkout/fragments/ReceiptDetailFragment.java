df96bb945123d549a724f4dc16887b5e94563af2
Suman <sersoun@gmail.com>
2015-04-05 18:36:33 -0700

    item reminder

--- src/main/java/com/receiptofi/checkout/fragments/ReceiptDetailFragment.java
+++ src/main/java/com/receiptofi/checkout/fragments/ReceiptDetailFragment.java
@@ -1,31 +1,50 @@
 package com.receiptofi.checkout.fragments;
 
+import android.app.DatePickerDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract.Calendars;
+import android.provider.CalendarContract.Reminders;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.receiptofi.checkout.R;
 import com.receiptofi.checkout.adapters.ReceiptItemListAdapter;
+import com.receiptofi.checkout.model.ReceiptItemModel;
 import com.receiptofi.checkout.model.ReceiptModel;
+import com.receiptofi.checkout.utils.Constants;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.StringTokenizer;
+import java.util.TimeZone;
 
 /**
  * User: PT
  * Date: 1/1/15 12:44 PM
  */
-public class ReceiptDetailFragment extends Fragment {
+public class ReceiptDetailFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private static final String TAG = ReceiptDetailFragment.class.getSimpleName();
 
@@ -33,6 +52,7 @@ public class ReceiptDetailFragment extends Fragment {
     public final static String ARG_POSITION = "position";
     int mCurrentIndex = -1;
     int mCurrentPosition = -1;
+    int mCurrItemIndex = -1;
 
     // Receipt detail biz info
     private TextView rdBizName;
@@ -51,6 +71,8 @@ public class ReceiptDetailFragment extends Fragment {
     private TextView taxAmountView;
     private TextView totalAmountView;
 
+    private List<ReceiptItemModel> itemList;
+    private static boolean dateSet = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -207,6 +229,38 @@ public class ReceiptDetailFragment extends Fragment {
 
             // Set Adaptor on items list
         rdItemsList.setAdapter(new ReceiptItemListAdapter(getActivity(), rdModel.getReceiptItems()));
+            itemList = rdModel.getReceiptItems();
+            if(Constants.SET_RECEIPT_REMINDER) {
+                rdItemsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        final Calendar c = Calendar.getInstance();
+                        DatePickerDialog dialog = new DatePickerDialog(getActivity(), ReceiptDetailFragment.this, c.get(Calendar.YEAR),
+                                c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+                        mCurrItemIndex = position - 1;
+                        String itemName = (itemList.get(mCurrItemIndex)).getName();
+                        Log.d(TAG, "item name is: " + itemName);
+                        dialog.setTitle(getActivity().getString(R.string.reminder_title) + " " + itemName);
+                        dialog.setButton(DialogInterface.BUTTON_NEGATIVE, getActivity().getString(R.string.reminder_cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // action cancelled- nothing to do
+                                dateSet = false;
+                            }
+                        });
+                        dialog.setButton(DialogInterface.BUTTON_POSITIVE, getActivity().getString(R.string.reminder_set), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // User has set date we need to add calendar event
+                                dateSet = true;
+                            }
+                        });
+                        dialog.setCanceledOnTouchOutside(true);
+                        dialog.show();
+                        return true;
+                    }
+                });
+            }
 
         // Update trackers
         mCurrentIndex = index;
@@ -229,5 +283,82 @@ public class ReceiptDetailFragment extends Fragment {
         // Save the current article selection in case we need to recreate the fragment
         outState.putInt(ARG_INDEX, mCurrentIndex);
         outState.putInt(ARG_POSITION, mCurrentPosition);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        if(!dateSet){
+            Log.d(TAG, "Cancelled event received in onDateSet- nothing to do");
+            return;
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, monthOfYear, dayOfMonth);
+
+        ReceiptItemModel itemModel = itemList.get(mCurrItemIndex);
+        ContentResolver cr = getActivity().getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(Events.DTSTART, cal.getTimeInMillis());
+        values.put(Events.DTEND, cal.getTimeInMillis());
+        values.put(Events.ALL_DAY, true);
+        values.put("eventTimezone", TimeZone.getDefault().getID());
+        values.put(Events.TITLE, getActivity().getString(R.string.event_title) + " " + itemModel.getName());
+        values.put(Events.ORGANIZER, getActivity().getString(R.string.event_organizer));
+        values.put(Events.DESCRIPTION, getActivity().getString(R.string.event_description_1) + itemModel.getName()
+                                     + getActivity().getString(R.string.event_description_2) + " " + rdBizName.getText() + " "
+                                     + getActivity().getString(R.string.event_description_3) + " " + rdDate.getText());
+        values.put(Events.HAS_ALARM, true);
+        values.put(Events.STATUS, Events.STATUS_CONFIRMED);
+
+        // TODO: fix me - 1. No calendar
+        // TODO:          2. Multiple calendar
+        Map<Long, String> calendarMap = getAllCalendars();
+        if(calendarMap != null && calendarMap.size() > 0){
+            Long calId = -1L;
+            for(Entry entry : calendarMap.entrySet()){
+                calId = (Long)entry.getKey();
+                break;
+            }
+            values.put(Events.CALENDAR_ID, calId);
+
+            Uri uri = cr.insert(Events.CONTENT_URI, values);
+            Long eventId = Long.parseLong(uri.getLastPathSegment());
+
+            // Add reminder
+            ContentValues reminders = new ContentValues();
+            reminders.put(Reminders.EVENT_ID, eventId);
+            reminders.put(Reminders.METHOD, Reminders.METHOD_ALERT);
+            reminders.put(Reminders.MINUTES, Constants.DEFAULT_REMINDER_TIME);
+
+            // in case we would want to do something with event later
+            Uri uri2 = cr.insert(Reminders.CONTENT_URI, reminders);
+
+            Toast.makeText(getActivity(), getActivity().getString(R.string.reminder_toast) + " " + itemModel.getName(), Toast.LENGTH_SHORT).show();
+
+        } else {
+            //TODO: add logic to show - register for calendar
+        }
+
+    }
+
+    private Map<Long, String> getAllCalendars(){
+        Map<Long, String> calendarMap = new LinkedHashMap<>();
+        String[] projection = new String[]{Calendars._ID,
+                                           Calendars.NAME,
+                                           Calendars.ACCOUNT_NAME,
+                                           Calendars.ACCOUNT_TYPE}; // Keeping account name a type in case we want to use later.
+
+        Cursor calCursor = getActivity().getContentResolver().query(Calendars.CONTENT_URI,
+                                                                    projection,
+                                                                    Calendars.VISIBLE + " = 1",
+                                                                    null,
+                                                                    Calendars._ID + " ASC");
+        if (calCursor.moveToFirst()) {
+            do {
+                calendarMap.put(calCursor.getLong(0), calCursor.getString(1));
+            } while (calCursor.moveToNext());
+        }
+        Log.d(TAG, "found calendars " + calendarMap.toString());
+        return calendarMap;
     }
 }
