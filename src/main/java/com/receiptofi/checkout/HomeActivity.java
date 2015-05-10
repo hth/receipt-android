2cf883f18fbe363865f6ca71e3d73ee392edf22f
Suman <sersoun@gmail.com>
2015-05-09 23:16:10 -0700

    Chart legend fix

--- src/main/java/com/receiptofi/checkout/HomeActivity.java
+++ src/main/java/com/receiptofi/checkout/HomeActivity.java
@@ -23,12 +23,14 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
@@ -41,6 +43,7 @@ import com.receiptofi.checkout.utils.AppUtils;
 import com.receiptofi.checkout.utils.Constants;
 import com.receiptofi.checkout.utils.db.KeyValueUtils;
 import com.receiptofi.checkout.utils.db.MonthlyReportUtils;
+import com.receiptofi.checkout.views.FlowLayout;
 
 import java.io.File;
 import java.text.DateFormat;
@@ -260,12 +263,6 @@ public class HomeActivity extends Activity implements OnChartValueSelectedListen
 
         mChart.setCenterText(getString(R.string.chart_desc_short));
         mChart.setCenterTextSize(12f);
-/*
-        Legend l = mChart.getLegend();
-        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(5f);
-        */
     }
 
     private void updateChartData() {
@@ -286,6 +283,7 @@ public class HomeActivity extends Activity implements OnChartValueSelectedListen
             mChart.highlightValues(null);
 
             mChart.invalidate();
+            addLegend();
         } else {
             mChart.setVisibility(View.GONE);
             emptyChart.setVisibility(View.VISIBLE);
@@ -304,38 +302,34 @@ public class HomeActivity extends Activity implements OnChartValueSelectedListen
             mChart.highlightValues(null);
 
             mChart.invalidate();
+            addLegend();
         } else {
             mChart.setVisibility(View.GONE);
             emptyChart.setVisibility(View.VISIBLE);
         }
     }
 
-    /*
-    private void updateChartData() {
-        if (expByBizAnimate) {
-            mChart.animateXY(1500, 1500);
-            // mChart.spin(2000, 0, 360);
-            expByBizAnimate = false;
+    private void addLegend(){
+        Legend legend = mChart.getLegend();
+        int[] colorCodes = legend.getColors();
+        String[] labelArr = legend.getLegendLabels();
+        legend.setEnabled(false);
+
+        FlowLayout ll = (FlowLayout)findViewById(R.id.legend_layout);
+        ll.removeAllViews();
+        for (int i = 0; i < colorCodes.length; i++){
+
+            LinearLayout legendLayout = (LinearLayout)LayoutInflater.from(this).inflate(R.layout.legend_item, null);
+
+            View legendColor = legendLayout.getChildAt(0);
+            legendColor.setBackgroundColor(colorCodes[i]);
+
+            TextView legendText = (TextView)legendLayout.getChildAt(1);
+            legendText.setText(labelArr[i]);
+
+            ll.addView(legendLayout);
         }
-
-        expByBizData = ChartService.getPieData();
-        mChart.setData(expByBizData);
-
-        // undo all highlights
-        mChart.highlightValues(null);
-
-        mChart.invalidate();
     }
-
-    private void setUpChartData() {
-        mChart.setData(expByBizData);
-
-        // undo all highlights
-        mChart.highlightValues(null);
-
-        mChart.invalidate();
-    }
-    */
 
     /**
      * On Pie selection.
