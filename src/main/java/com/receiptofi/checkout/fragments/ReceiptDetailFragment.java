8fdb23df59b19721b6ecb4e3c5360150c06a839b
Hitender Tholia <tholiahitender@gmail.com>
2015-05-11 19:47:04 -0700

    optimized import

--- src/main/java/com/receiptofi/checkout/fragments/ReceiptDetailFragment.java
+++ src/main/java/com/receiptofi/checkout/fragments/ReceiptDetailFragment.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.DatePicker;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
