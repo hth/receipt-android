7a6ab0ead788ae4eb59db7c2f5e7e58fa121b829
Suman <sersoun@gmail.com>
2015-05-15 10:00:29 -0700

    fix in process of files in queue and upload process

--- src/main/java/com/receiptofi/checkout/receivers/NetworkConnectivityReceiver.java
+++ src/main/java/com/receiptofi/checkout/receivers/NetworkConnectivityReceiver.java
@@ -4,17 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.receiptofi.checkout.adapters.ImageUpload;
+import com.receiptofi.checkout.model.ImageModel;
 import com.receiptofi.checkout.service.ImageUploaderService;
 import com.receiptofi.checkout.utils.UserUtils;
+
+import java.util.ArrayList;
 
 public class NetworkConnectivityReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
-
-        if (context != null) {
-            if (UserUtils.UserSettings.isStartImageUploadProcess(context)) {
+        
+        ArrayList<ImageModel> queue = ImageUpload.getImageQueue();
+        if (queue != null && !queue.isEmpty()) {
+            if (context != null && UserUtils.UserSettings.isStartImageUploadProcess(context)) {
                 ImageUploaderService.start(context);
             }
         }
