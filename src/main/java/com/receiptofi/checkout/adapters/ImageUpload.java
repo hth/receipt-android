02d95fcf3d69e33849b57ba7a8b270434662162d
Hitender Tholia <tholiahitender@gmail.com>
2015-06-29 12:39:20 -0700

    PMD fix

--- src/main/java/com/receiptofi/checkout/adapters/ImageUpload.java
+++ src/main/java/com/receiptofi/checkout/adapters/ImageUpload.java
@@ -1,11 +1,13 @@
 package com.receiptofi.checkout.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.database.sqlite.SQLiteConstraintException;
 import android.os.Message;
 import android.util.Log;
 
 import com.receiptofi.checkout.MainMaterialDrawerActivity;
+import com.receiptofi.checkout.R;
 import com.receiptofi.checkout.ReceiptofiApplication;
 import com.receiptofi.checkout.fragments.HomeFragment;
 import com.receiptofi.checkout.model.ImageModel;
@@ -17,6 +19,9 @@ import java.util.ArrayList;
 public class ImageUpload {
 
     public static ArrayList<ImageModel> imageQueue = null;
+
+    private ImageUpload() {
+    }
 
     public static void initializeQueue() {
         if (imageQueue == null) {
@@ -40,20 +45,21 @@ public class ImageUpload {
             isAddedToDB = model.addToQueue();
             imageQueue.add(model);
 
-            if (!ImageUploaderService.isServiceConnected() && isAddedToDB && UserSettings.isStartImageUploadProcess(context)) {
+            if (!ImageUploaderService.isServiceConnected() &&
+                    isAddedToDB &&
+                    UserSettings.isStartImageUploadProcess(context)) {
                 Log.i("image added to queue", model.imgPath);
                 ImageUploaderService.start(context);
             } else {
                 Message msg = new Message();
                 msg.what = HomeFragment.IMAGE_ADDED_TO_QUEUED;
-                msg.obj = "Image added to queue. Upload start automatically when network is available.";
+                msg.obj = context.getApplicationContext().getString(R.string.image_added_to_queue);
                 ((MainMaterialDrawerActivity) context).mHomeFragment.updateHandler.sendMessage(msg);
             }
         } catch (SQLiteConstraintException e) {
             Message msg = new Message();
             msg.what = HomeFragment.IMAGE_ALREADY_QUEUED;
-            //TODO(hth) move this to XML file
-            msg.obj = "This image already exists in upload queue.";
+            msg.obj = context.getApplicationContext().getString(R.string.image_exists_in_queue);
             if (ReceiptofiApplication.isHomeActivityVisible()) {
                 ((MainMaterialDrawerActivity) context).mHomeFragment.updateHandler.sendMessage(msg);
             }
