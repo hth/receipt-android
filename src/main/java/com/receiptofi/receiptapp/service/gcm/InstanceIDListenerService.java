1cca6dec75f0c4ebe3d5207d9eb7f09abbf6418f
hth <hth@users.noreply.github.com>
2016-12-02 17:03:50 -0800

    Started with FCM

--- src/main/java/com/receiptofi/receiptapp/service/gcm/InstanceIDListenerService.java
+++ src/main/java/com/receiptofi/receiptapp/service/gcm/InstanceIDListenerService.java
@@ -1,25 +1,34 @@
 package com.receiptofi.receiptapp.service.gcm;
 
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
  * User: hitender
  * Date: 8/28/15 11:40 PM
  */
-public class InstanceIDListenerService extends com.google.android.gms.iid.InstanceIDListenerService {
+public class InstanceIDListenerService extends FirebaseInstanceIdService {
     private static final String TAG = InstanceIDListenerService.class.getSimpleName();
 
     /**
      * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. This call is initiated by the
-     * InstanceID provider.
+     * the previous token had been compromised. Note that this is also called
+     * when the InstanceID token is initially generated, so this is where
+     * you retrieve the token.
      */
     // [START refresh_token]
     @Override
     public void onTokenRefresh() {
-        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+
         Intent intent = new Intent(this, RegistrationIntentService.class);
         startService(intent);
     }
-    // [END refresh_token]
 }
